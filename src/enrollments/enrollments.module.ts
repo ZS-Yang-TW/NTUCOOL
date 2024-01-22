dba38767e583b9b49f8b69bbee0745d65d1a0a2e
steven <msycomtw@gmail.com>
2024-01-22 18:40:40 +0800

    feat: revise course, enrollment moudle

--- src/enrollments/enrollments.module.ts
+++ src/enrollments/enrollments.module.ts
@@ -7,4 +7,4 @@ import { EnrollmentsService } from './enrollments.service';
   providers: [EnrollmentsService],
   exports: [EnrollmentsService],
 })
-export class UsersModule {}
+export class EnrollmentsModule {}
